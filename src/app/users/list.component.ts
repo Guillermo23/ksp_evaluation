d44122d001e9c4a3ebb31ecdbe91a84748cb0a54
Guillermo Carapia <carapiag@gmail.com>
2023-03-16 09:53:41 -0600

    se integra funcionalidad para exportacion de datos

--- src/app/users/list.component.ts
+++ src/app/users/list.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+
 import { first } from 'rxjs/operators';
+import * as XLSX from 'xlsx';
 
 import { UserService } from '../_services';
 import { User } from '../_models';
@@ -12,6 +14,8 @@ import { User } from '../_models';
 
 export class ListComponent implements OnInit {
     users!: User[];
+    @ViewChild('TABLE') table: ElementRef | undefined;
+    displayedColumns = ['position', 'name', 'weight', 'symbol'];
 
     constructor(private userService: UserService) {}
 
@@ -19,6 +23,15 @@ export class ListComponent implements OnInit {
         this.userService.getAll()
             .pipe(first())
             .subscribe(users => this.users = users);
+    }
+
+    exportTOExcel() {
+        const ws: XLSX.WorkSheet=XLSX.utils.table_to_sheet(this.table?.nativeElement);
+        const wb: XLSX.WorkBook = XLSX.utils.book_new();
+        XLSX.utils.book_append_sheet(wb, ws, 'Sheet1');
+        
+        /* save to file */
+        XLSX.writeFile(wb, 'KSP_Empleados.xlsx');
     }
 
     deleteUser(id: string) {
